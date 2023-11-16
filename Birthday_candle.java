6f317bc756afbe001b36df1bebadec8fb3ffbec0
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-11-16 21:59:24 +0530

    Create Birthday_candle.java

---
+++ Birthday_candle.java
@@ -0,0 +1,48 @@
+//Birthday Cake Candles
+/*Sample Input 0
+
+4
+3 2 1 3
+Sample Output 0
+
+2*/
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    // Write your code here
+      int max=Collections.max(candles);
+        long count=candles.stream().filter(height->height==max).count();
+        
+        return (int)count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
