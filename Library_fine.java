0f1379f37c15617d573de45454b40a728f4a721e
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-11-20 22:08:24 +0530

    Library_fine.java

---
+++ Library_fine.java
@@ -0,0 +1,72 @@
+//Library Fine
+/*Sample Input
+
+9 6 2015
+6 6 2015
+Sample Output
+
+45*/
+
+
+
+class Result {
+
+    /*
+     * Complete the 'libraryFine' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER d1
+     *  2. INTEGER m1
+     *  3. INTEGER y1
+     *  4. INTEGER d2
+     *  5. INTEGER m2
+     *  6. INTEGER y2
+     */
+
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+    // Write your code here
+      if(y1<y2 || (y1==y2 && m1<m2) || (y1==y2 && m1==m2 && d1<=d2)){
+          return 0;
+      } else if(y1==y2 && m1==m2){
+          return 15*(d1-d2);
+      } else if(y1==y2 && m1>m2){
+          return 500*(m1-m2);
+      }else{
+          return 10000;
+      }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int m1 = Integer.parseInt(firstMultipleInput[1]);
+
+        int y1 = Integer.parseInt(firstMultipleInput[2]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d2 = Integer.parseInt(secondMultipleInput[0]);
+
+        int m2 = Integer.parseInt(secondMultipleInput[1]);
+
+        int y2 = Integer.parseInt(secondMultipleInput[2]);
+
+        int result = Result.libraryFine(d1, m1, y1, d2, m2, y2);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
