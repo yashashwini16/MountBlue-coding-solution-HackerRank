4b68f23213abe509173ee08c598a2ab9b37b77ca
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-11-25 22:32:26 +0530

    camelcase.java

---
+++ camelcase.java
@@ -0,0 +1,47 @@
+//Camel Case problem
+/*Sample Input
+
+saveChangesInTheEditor
+Sample Output
+
+5*/
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+    // Write your code here
+        int word=1;
+        for(char character:s.toCharArray()){
+            if(Character.isUpperCase(character)){
+                word++;
+            }
+        }
+        return word;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
