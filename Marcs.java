f36c635d1fda51425a33ae1da87c7ce1ec0c671e
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-11-27 20:03:24 +0530

    Marcs.java

---
+++ Marcs.java
@@ -0,0 +1,52 @@
+//Marc's Cakewalk
+/*Sample Input 0
+
+3
+1 3 2
+Sample Output 0
+
+11*/
+
+class Result {
+
+    /*
+     * Complete the 'marcsCakewalk' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY calorie as parameter.
+     */
+
+    public static long marcsCakewalk(List<Integer> calorie) {
+    // Write your code here
+        Collections.sort(calorie,Collections.reverseOrder());
+        int n=calorie.size();
+        long miles=0;
+        for(int i=0;i<n;i++){
+            miles+=Math.pow(2,i)*calorie.get(i);
+        }
+        return miles;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> calorie = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        long result = Result.marcsCakewalk(calorie);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
