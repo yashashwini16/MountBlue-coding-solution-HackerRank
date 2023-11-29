98605d5b88c29ef43c8d284edfeed61a7ad464bd
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-11-29 20:24:42 +0530

    beautiful.java

---
+++ beautiful.java
@@ -0,0 +1,62 @@
+//Beautiful days at the movies
+
+
+
+class Result {
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k) {
+    // Write your code here
+        int beautic=0;
+        for(int day=i;day<=j;day++){
+            int reversed=reverse(day);
+            int absolute=Math.abs(day-reversed);
+            if(absolute%k==0){
+                beautic++;
+            }
+        }
+        return beautic;
+
+    }
+    private static int reverse(int num){
+        int rev=0;
+        while(num!=0){
+            rev=rev*10+num%10;
+            num=num/10;
+        }
+        return rev;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
