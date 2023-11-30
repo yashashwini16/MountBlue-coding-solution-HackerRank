b231527fd19a4c54075faff2033c0ab0c76f4925
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-11-30 22:17:14 +0530

    Sequence.java

---
+++ Sequence.java
@@ -0,0 +1,59 @@
+//Sequence Equation
+/*Sample Input 0
+
+3
+2 3 1
+Sample Output 0
+
+2
+3
+1*/
+
+
+class Result {
+
+    /*
+     * Complete the 'permutationEquation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY p as parameter.
+     */
+
+    public static List<Integer> permutationEquation(List<Integer> p) {
+    // Write your code here
+        List<Integer> result=new ArrayList<>();
+        for(int x=1;x<=p.size();x++){
+            int indx=p.indexOf(x)+1;
+            int indy=p.lastIndexOf(indx)+1;
+            result.add(indy);
+        }
+        return result;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> p = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.permutationEquation(p);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
