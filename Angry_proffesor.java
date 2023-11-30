938b7cd5f59d7b9aace76a4170968c08c33d402b
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-11-30 22:26:40 +0530

    Angry_proffesor.java

---
+++ Angry_proffesor.java
@@ -0,0 +1,71 @@
+//Angry Professor
+/*Sample Input
+
+2
+4 3
+-1 -3 4 2
+4 2
+0 -1 2 1
+Sample Output
+
+YES
+NO*/
+
+
+class Result {
+
+    /*
+     * Complete the 'angryProfessor' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY a
+     */
+
+    public static String angryProfessor(int k, List<Integer> a) {
+    // Write your code here
+        int ontime=0;
+        for(int arrive:a){
+            if(arrive<=0){
+                ontime++;
+            }
+        }
+        return (ontime>=k)?"NO":"YES";
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int k = Integer.parseInt(firstMultipleInput[1]);
+
+                List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = Result.angryProfessor(k, a);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
