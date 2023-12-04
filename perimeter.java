88340565dacca741b32b343bb26267efac03fb02
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-12-04 16:37:52 +0530

    perimeter.java

---
+++ perimeter.java
@@ -0,0 +1,74 @@
+//Maximum perimeter trianlge
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'maximumPerimeterTriangle' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY sticks as parameter.
+     */
+
+    public static List<Integer> maximumPerimeterTriangle(List<Integer> sticks) {
+    // Write your code here
+        List<Integer> res=new ArrayList<>();
+        sticks.sort(Collections.reverseOrder());
+        int n=sticks.size();
+        boolean found=false;
+        for(int i=0;i<n-2;i++){
+            int s1=sticks.get(i);
+            int s2=sticks.get(i+1);
+            int s3=sticks.get(i+2);
+            if(s1<s2+s3){
+                res.add(s3);
+                res.add(s2);
+                res.add(s1);
+                found=true;
+                break;
+            }
+        }
+        if(!found){
+            res.add(-1);
+        }
+        return res;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> sticks = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.maximumPerimeterTriangle(sticks);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
