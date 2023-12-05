89dd19f43489107f53a8d626233e384aaa45bcfa
Yashaswini B <107267601+yashashwini16@users.noreply.github.com>
2023-12-05 18:16:03 +0530

    beautiful_binary.java

---
+++ beautiful_binary.java
@@ -0,0 +1,54 @@
+//Beautiful binary string
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulBinaryString' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING b as parameter.
+     */
+
+    public static int beautifulBinaryString(String b) {
+    // Write your code here
+        int count=0;
+        while(b.contains("010")){
+            b=b.replaceFirst("010","011");
+            count++;
+        }
+        return count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String b = bufferedReader.readLine();
+
+        int result = Result.beautifulBinaryString(b);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
